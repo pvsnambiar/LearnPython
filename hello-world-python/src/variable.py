c1a93905a173edd09114bc1f42892749b69013e7
pvsnambiar <pvs.common@gmail.com>
2025-01-08 19:42:40 -0500

    different variable

---
+++ hello-world-python/src/variable.py
@@ -0,0 +1,41 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John" 
+x = 5 # assign a integer to a variable
+y = "John" # assign a string to a variable
+a, b ,c = "Orange", "Banana", "Cherry";# assign multiple values to multiple variables
+d= e = f = "Orange" # assign the same value to multiple variables
+fruits = ["apple", "banana", "cherry"] # assign multiple values to multiple variables
+x1, y1, z1 = fruits # assign the values of an array to multiple variables
+s = 5
+t = 10
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+print(x)
+print(y)  
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)    
+print(f)    
+print(x1)
+print(y1)
+print(z1) 
+print(a, b, c, d, e, f, x1, y1, z1) # print multiple variables   
+print(s + t) # print the sum of two variables
+print (x + s) # print the sum of two variables
+print (x + t) # print the sum of two variables
+print (x + s + t) # print the sum of two variables
+print (x + s + t + s)
+print (x-t)
+print (x , y) 
